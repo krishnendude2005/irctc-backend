4ec3016c293c48e953c5c8952e5ffc6968c24051
Krishnendu De <krishnendu1205.be22@chitkarauniversity.edu.in>
2025-11-07 13:34:35 +0530

    Added more user services

--- src/main/java/ticket/booking/services/UserBooking.java
+++ src/main/java/ticket/booking/services/UserBooking.java
@@ -3,10 +3,13 @@ package ticket.booking.services;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ticket.booking.Ticket;
+import ticket.booking.Train;
 import ticket.booking.User;
+import ticket.booking.utils.UserServiceUtil;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,19 +17,42 @@ public class UserBooking {
     private User user;
     private List<User> userList;
 
-    private static final String USERS_PATH = "../localdb/users";
+    private static final String LOCALDB_PATH = "../localdb";
     private ObjectMapper objectMapper = new ObjectMapper();
 
     // create a constructor for - initializing a logged user and making the user global level so anyone can access the user
     public UserBooking(User user) throws IOException {
         this.user = user;
-        File users = new File(USERS_PATH);
+        loadUsers();
+    }
+
+    public UserBooking() throws IOException {
+        loadUsers();
+    }
+
+    public List<User> loadUsers() throws IOException {
+        File users = new File(LOCALDB_PATH + "/users.json");
         userList = objectMapper.readValue(users, new TypeReference<List<User>>() {
+        });
+        return userList;
+    }
+
+    public void saveUserToFile() throws IOException {
+        File userFile = new File(LOCALDB_PATH + "/users.json");
+        objectMapper.writeValue(userFile, userList);
+
+    }
+
+    public List<Train> loadTrains() throws IOException {
+        File trains = new File(LOCALDB_PATH + "/trains.json");
+        return objectMapper.readValue(trains, new TypeReference<List<Train>>() {
         });
     }
 
+    // Features a user can use
+
     public Boolean loginUser() {
-        Optional<User> loggedUser = userList.stream().filter(user1 -> user1.getName().equals(user.getName()) && user1.getHashedPassword().equals(user.getHashedPassword())).findFirst();
+        Optional<User> loggedUser = userList.stream().filter(user1 -> user1.getName().equals(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(), user1.getHashedPassword())).findFirst();
         if (loggedUser.isPresent()) {
             System.out.println("Login Successful");
             return true;
@@ -42,22 +68,18 @@ public class UserBooking {
             saveUserToFile();
             System.out.println("Signup Successful");
             return Boolean.TRUE;
-        }catch(Exception e) {
+        } catch (Exception e) {
             System.out.println("SignUp Failed");
             e.printStackTrace();
         }
         return Boolean.FALSE;
     }
 
-    public void saveUserToFile() throws IOException {
-        File userFile = new File(USERS_PATH);
-        objectMapper.writeValue(userFile, userList);
-
-    }
 
     public void fetchBooking() {
         this.user.PrintTicketsBooked();
     }
+
     public boolean cancelBooking(String ticketId) throws IOException {
         // Step 1: Find the ticket in the logged-in user's bookings
         Optional<Ticket> ticketToCancel = user.getTicketsBooked()
@@ -82,6 +104,15 @@ public class UserBooking {
         return false;
     }
 
+    public List<Train> searchTrains(String source, String destination) throws IOException {
+        List<Train> trains = new ArrayList<>();
+        for (Train train : loadTrains()) {
+            if (train.getStations().contains(source) && train.getStations().contains(destination)) {
+                trains.add(train);
+            }
+        }
+        return trains;
+    }
 
 }
 
